2cf50b698a657a3f0ba317771ac1562095a4be14
mgh <294913557@qq.com>
2019-11-02 16:29:13 +0800

    xadmin的文件

--- kexie/kexie/settings.py
+++ kexie/kexie/settings.py
@@ -16,7 +16,7 @@ import  sys
 pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,os.path.join(BASE_DIR,'apps_extra'))
+#sys.path.insert(0,os.path.join(BASE_DIR,'apps_extra'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/pip
